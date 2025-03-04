a61835a580565ccd93b287c96bd45a3e70c8cb0f
Idan Bunimovich <idan@prismaphotonics.com>
2025-03-04 10:41:44 +0200

    added fixes to deploy shaked airflow and postgres

--- stacks/shaked-stack/airflow/scripts/add_airflow_prerequisites.sh
+++ stacks/shaked-stack/airflow/scripts/add_airflow_prerequisites.sh
@@ -27,19 +27,24 @@ copy_files() {
     cp -r /home/prisma/"$OFFLINE_DEPLOY"/shaked/coordinates/* /home/prisma/shaked/coordinates/ || error_exit "Failed to copy contents from /home/prisma/$OFFLINE_DEPLOY/shaked/coordinates to /home/prisma/shaked/coordinates"
 }
 
-add_secrets() {
-    kubectl create secret generic shaked-onprem-secret \
-        --from-literal=db_host="postgres-shaked.postgres-shaked.svc.cluster.local" \
-        --from-literal=db_username="postgres" \
-        --from-literal=db_password="postrgres" \
-        -n airflow || error_exit "Failed to create secret shaked-onprem-secret in namespace airflow"
+add_pvs() {
+    kubectl apply -f airflow/pvs.yml || error_exit "Failed to apply persistent volumes from airflow/pvs.yml"
 }
 
-add_pvs() {
-    kubectl apply -f ../airflow/pvs.yml || error_exit "Failed to apply persistent volumes from ../airflow/pvs.yml"
+add_secrets() {
+    if kubectl get secret shaked-onprem-secret -n airflow >/dev/null 2>&1; then
+        log "Secret 'shaked-onprem-secret' already exists in namespace 'airflow'. Skipping creation."
+    else
+        kubectl create secret generic shaked-onprem-secret \
+            --from-literal=db_host="postgres-shaked-postgresql.postgres-shaked.svc.cluster.local" \
+            --from-literal=db_username="postgres" \
+            --from-literal=db_password="postrgres" \
+            -n airflow || error_exit "Failed to create secret 'shaked-onprem-secret' in namespace 'airflow'"
+        log "Secret 'shaked-onprem-secret' created successfully."
+    fi
 }
 
 create_folder_structure
 copy_files
-add_secrets
 add_pvs
+add_secrets
